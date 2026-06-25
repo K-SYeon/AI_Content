884541c5e89bddae510edff3c44d7a1facf6860f
K-SYeon <thdus0539@naver.com>
2026-06-25 14:21:28 +0900

    Add generate button event handler

--- 05/common/js/ui.js
+++ 05/common/js/ui.js
@@ -2,15 +2,47 @@ const UI = {
 
     init() {
 
-        console.log('UI Init');
-
         this.bindEvents();
 
     },
 
     bindEvents() {
 
-        console.log('UI Event Bind');
+        $('.btn-generate').on('click', () => {
+
+            this.showSampleResult();
+
+        });
+
+    },
+
+    showSampleResult() {
+
+        const sampleData = [
+            {
+                question: '0.3 + 0.5 = ?'
+            },
+            {
+                question: '0.7 + 0.2 = ?'
+            },
+            {
+                question: '0.8 + 0.4 = ?'
+            }
+        ];
+
+        let html = '';
+
+        sampleData.forEach((item, index) => {
+
+            html += `
+                <div class="question-item">
+                    문제 ${index + 1}. ${item.question}
+                </div>
+            `;
+
+        });
+
+        $('.result-list').html(html);
 
     }
 
